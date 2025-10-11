016d268b1e041a5d0b018d2e79e70dcf8cb22b93
Eric Charnesky <2469199+EricCharnesky@users.noreply.github.com>
2025-10-11 01:51:35 -0400

    happy friday

--- Week7/hello.js
+++ Week7/hello.js
@@ -3,6 +3,7 @@ const http = require('http');
 const server = http.createServer(function ( request, response ) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello world from node.js");
+    response.write("\nHappy Friday!")
     response.end();
 });
 
